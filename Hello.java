b6b6109e6e6c7e772d694bda1eb7ecec193fa97a
himaja4002 <himaja111@gmail.com>
2018-09-07 14:35:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,47 @@
+class Hello{  
+public static void main(String[] args){  
+for(int i=1;i<=10;i++){
+System.out.println(i);
+}
+}
+}
+
+
+
+Note: Regarding Q10, date/time api gave only time and date. TO display year, I have used date value and made js operations on it . HOpe its fine.
+
+kvnarayanarao@gmail.com
+
+***********************************************************************************************************
+hositing
+	variable, function
+closure
+bind vs call vs apply
+
+
+implement setInterval by using setTimout
+js:array filter/ map
+promise js
+event listerns (event bubbling , event capturing)
+event immediate propagation, stopimmediate propvagation, prevent default, return false, void(0)
+css
+how to allign a div center of a window
+margin collapsing
+border box
+
+inline vs inline blcok
+specifity
+
+async vs defer
+spreaad  operetor 
+
+throttle , devouncing
+document ready vs window.load
+web workers
+local storate vs session storate
+
+
+
+
+
+
---
+++ Student3.java
@@ -0,0 +1,25 @@
+class Student3{
+int marks;
+String name;
+void insertMarks(int m, string n){
+marks=m;
+name=n;
+}
+void displayInformation(){
+System.out.println(marks+" "+name);
+}
+public static void main(string args[]) {
+Student3 S1= new Student3();
+Student3 S2=new Student3();
+S1.insertMarks(95,"Madhu");
+S2.insertmarks(100,"sudhan");
+
+S1.displayInformation();
+S2.displayInformatiion();
+}
+}
+
+
+
+
+
