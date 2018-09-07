0537db82d5632b0dbafcdb43643cc338f5a081e4
Himaja <himaja111@gmail.com>
2018-09-07 16:49:53 -0400

    Deleted Comments

--- Hello.java
+++ Hello.java
@@ -9,38 +9,6 @@ System.out.println(i+1);
 
 
 
-Note: Regarding Q10, date/time api gave only time and date. TO display year, I have used date value and made js operations on it . HOpe its fine.
-
-kvnarayanarao@gmail.com
-
-***********************************************************************************************************
-hositing
-	variable, function
-closure
-bind vs call vs apply
-
-
-implement setInterval by using setTimout
-js:array filter/ map
-promise js
-event listerns (event bubbling , event capturing)
-event immediate propagation, stopimmediate propvagation, prevent default, return false, void(0)
-css
-how to allign a div center of a window
-margin collapsing
-border box
-
-inline vs inline blcok
-specifity
-
-async vs defer
-spreaad  operetor 
-
-throttle , devouncing
-document ready vs window.load
-web workers
-local storate vs session storate
-
 
 
 
